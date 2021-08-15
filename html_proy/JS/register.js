1de4a6b55d817027cd0f58307257f5ac5190d113
Esteban Lopez Fernandez <40614256+EstebanLopezF@users.noreply.github.com>
2021-08-14 22:22:26 -0600

    Add frontend POST

--- html_proy/JS/register.js
+++ html_proy/JS/register.js
@@ -1,8 +1,11 @@
 $( "#buttom_register" ).click(function() {
 
+        let allok = 0;
+
         if(document.getElementById("serviceName").value.length == 0)
         {
             document.getElementById("serviceName").classList.add("errorinput");
+            allok = 1;
         } else {
             document.getElementById("serviceName").classList.remove("errorinput");
         }
@@ -10,6 +13,7 @@ $( "#buttom_register" ).click(function() {
         if(document.getElementById("chargeName").value.length == 0)
         {
             document.getElementById("chargeName").classList.add("errorinput");
+            allok = 1;
         } else {
             document.getElementById("chargeName").classList.remove("errorinput");
         }
@@ -17,6 +21,7 @@ $( "#buttom_register" ).click(function() {
         if(document.getElementById("service").value.length == 0)
         {
             document.getElementById("service").classList.add("errorinput");
+            allok = 1;
         } else {
             document.getElementById("service").classList.remove("errorinput");
         }
@@ -24,6 +29,7 @@ $( "#buttom_register" ).click(function() {
         if(document.getElementById("phoneNum").value.length == 0)
         {
             document.getElementById("phoneNum").classList.add("errorinput");
+            allok = 1;
         } else {
             document.getElementById("phoneNum").classList.remove("errorinput");
         }
@@ -31,6 +37,7 @@ $( "#buttom_register" ).click(function() {
         if(document.getElementById("email").value.length == 0)
         {
             document.getElementById("email").classList.add("errorinput");
+            allok = 1;
         } else {
             document.getElementById("email").classList.remove("errorinput");
         }
@@ -38,9 +45,37 @@ $( "#buttom_register" ).click(function() {
         if(document.getElementById("description").value.length == 0)
         {
             document.getElementById("description").classList.add("errorinput");
+            allok = 1;
         } else {
             document.getElementById("description").classList.remove("errorinput");
         }
+
+        if (allok == 0){
+
+            var data = JSON.stringify({"Name":document.getElementById("serviceName").value
+            ,"PhoneNumber":document.getElementById("phoneNum").value
+            ,"Email":document.getElementById("email").value
+            ,"Contact":document.getElementById("chargeName").value
+            ,"Type":document.getElementById("service").value
+            ,"Description":document.getElementById("description").value}); 
+
+            console.log(data);
+
+            xhr = new XMLHttpRequest(); 
+            var url = "https://localhost:44346/api/Service"; 
+            xhr.open("POST", url, true); 
+            xhr.setRequestHeader("Content-type", "application/json"); 
+            xhr.onreadystatechange = function () {  
+                if (xhr.readyState == 4 && xhr.status == 200) { 
+                    var json = JSON.parse(xhr.responseText); 
+                    console.log(json.email + ", " + json.name) 
+                } 
+            } 
+            xhr.send(data)
+
+
+
+        } 
         
   });
 
